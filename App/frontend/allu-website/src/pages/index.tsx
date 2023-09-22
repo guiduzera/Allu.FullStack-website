897f6e86ac7261a715cbce636469942de085f6aa
Guilherme Carvalho <guigui.carvalho15@gmail.com>
2023-09-22 19:03:55 -0300

    FEAT: Adicionando Head à página de produtos

--- App/frontend/allu-website/src/pages/index.tsx
+++ App/frontend/allu-website/src/pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <HomeContainer>
       <Head>
-        <title>Home | Profile</title>
+        <title>Allu | Profile</title>
         <meta name="description" content="informaçoes gerais do perfil" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/logo.png" />
--- App/frontend/allu-website/src/pages/products/index.tsx
+++ App/frontend/allu-website/src/pages/products/index.tsx
@@ -3,10 +3,17 @@ import ProductsCatalog from "@/components/ProductsCatalog";
 import ProductsFilter from "@/components/ProductsCatalog/ProductsFilter";
 import SearchBar from "@/components/SearchBar";
 import { ProductsCatalogContainer, ProductsContainer } from "@/styles/productsStyles";
+import Head from "next/head";
 
 export default function Products() {
   return (
     <ProductsContainer>
+      <Head>
+        <title>Allu | Products</title>
+        <meta name="description" content="loja compleata de produtos" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/logo.png" />
+      </Head>
       <Header />
       <SearchBar />
       <ProductsFilter />
