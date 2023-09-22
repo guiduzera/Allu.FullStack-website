bc36827b3f27eacc27541b3c6b81c4eaea1b57a6
Guilherme Carvalho <guigui.carvalho15@gmail.com>
2023-09-21 21:04:35 -0300

    CONFIG: Configurando a lib styled components com estilo global, thema de cores e etc... E instalando mais algumas libs para efeitos css

--- App/frontend/allu-website/src/pages/_app.tsx
+++ App/frontend/allu-website/src/pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/globals.css'
+import theme from '@/styles/theme'
+import GlobalStyles from '@/styles/global'
+import NextNProgress from 'nextjs-progressbar'
+import { Toaster } from 'react-hot-toast';
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <NextNProgress
+        color={theme.primary}
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow
+      />
+      <Toaster position="bottom-right" />
+      <Component {...pageProps} />
+      <GlobalStyles />
+    </ThemeProvider>
+  )
 }
---
+++ App/frontend/allu-website/src/styles/theme.ts
@@ -0,0 +1,8 @@
+const theme = {
+  background: '#ffffff',
+  componentsBackground: 'rgb(45, 45, 56)',
+  primary: 'rgb(39, 174, 96)',
+  error: '#d45d5d'
+};
+
+export default theme;
