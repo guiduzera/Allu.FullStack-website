bca76bb38d68e10b9e397bd900c4e8e4a782c16f
Guilherme Carvalho <guigui.carvalho15@gmail.com>
2023-09-21 21:27:17 -0300

    ADD: Adicionando meta dados na página home

--- App/frontend/allu-website/src/styles/HomeStyles.ts
+++ App/frontend/allu-website/src/styles/HomeStyles.ts
@@ -3,10 +3,12 @@ import styled from 'styled-components';
 export const HomeContainer = styled.div`
   display: flex;
   flex-direction: column;
-  width: 100%;
-  height: 100%;
+  width: 100vw;
+  height: 100vh;
 
   > main {
+    height: 100%;
+    width: 100%;
     display: flex;
     flex-direction: column;
     gap: 8rem;
