55947a6d2c0b72b5d515bea55eec01a4ef74669d
Guilherme Carvalho <guigui.carvalho15@gmail.com>
2023-09-24 02:17:07 -0300

    FEAT: Instalano cors e configurando os serviços para rodarem com docker

---
+++ App/backend/security/src/app.ts
@@ -0,0 +1,37 @@
+import express, { Express } from 'express';
+import cors from 'cors';
+// import Router from './routes/Router';
+// import UniversalErrorMiddleware from './middlewares/UniversalErrorMiddleware';
+
+class App {
+  public app: Express;
+//   public routerClass = new Router();
+
+  constructor() {
+    this.app = express();
+
+    this.config();
+
+    this.app.get('/', (req, res) => {
+      res.status(200).json({ message: 'Hello World!' });
+    });
+  }
+
+  private config(): void {
+    this.app.use(express.json());
+    this.app.use(cors());
+    // this.app.use(this.routerClass.router);
+    // this.app.use(UniversalErrorMiddleware.handleErrors);
+  }
+
+  public start(PORT: number | string): void {
+    this.app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  }
+}
+
+export { App };
+
+// importação necessária para o teste
+export const { app } = new App();
---
+++ App/backend/security/src/server.ts
@@ -0,0 +1,8 @@
+import { App } from './app';
+import 'dotenv/config';
+
+const PORT = process.env.APP_PORT || 3002;
+
+const app = new App();
+
+app.start(PORT);
