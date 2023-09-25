fb98c8280197d87b1bf8bba660eb7c3ffc049d36
Guilherme Carvalho <guigui.carvalho15@gmail.com>
2023-09-24 23:41:55 -0300

    FIX: Deixando método prismaClient privado

--- App/backend/security/src/model/security.model.ts
+++ App/backend/security/src/model/security.model.ts
@@ -2,15 +2,15 @@ import { PrismaClient, User } from '@prisma/client';
 import { ISecurityModel, IUserRegister } from '../interfaces/security.interfaces';
 
 export default class SecurityModel implements ISecurityModel {
-  public prismaClient: PrismaClient;
+  private _prismaClient: PrismaClient;
 
   constructor(prismaClient: PrismaClient) {
-    this.prismaClient = prismaClient;
+    this._prismaClient = prismaClient;
   }
 
   public async register(registerReq: IUserRegister): Promise<number | null> {
     const { name, email, password } = registerReq;
-    const create = await this.prismaClient.user.create({
+    const create = await this._prismaClient.user.create({
       data: { name, email, password }
     });
 
@@ -18,7 +18,7 @@ export default class SecurityModel implements ISecurityModel {
   }
 
   public async getUserByEmail(email: string): Promise<User | null> {
-    const user = await this.prismaClient.user.findUnique({ where: { email } });
+    const user = await this._prismaClient.user.findUnique({ where: { email } });
 
     return user;
   }
