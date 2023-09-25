9628ad083efef47d527d530ec6f3e7949e029d2d
Guilherme Carvalho <guigui.carvalho15@gmail.com>
2023-09-24 23:51:33 -0300

    CHANGE: Trocando visibilidade das variavéis para private

--- App/backend/security/src/services/security.service.ts
+++ App/backend/security/src/services/security.service.ts
@@ -3,18 +3,18 @@ import { IJwtPayload, ISecurityService, IUserLogin, IUserRegister } from '../int
 import { IBycript, IJwt, ISecurityModel } from '../interfaces/security.interfaces';
 
 export default class SecurityService implements ISecurityService {
-  public securityModel: ISecurityModel;
-  public jwt: IJwt;
-  public bcrypt: IBycript;
+  private _securityModel: ISecurityModel;
+  private _jwt: IJwt;
+  private _bcrypt: IBycript;
 
   constructor(securityModel: ISecurityModel, jwt: IJwt, bcrypt: IBycript) {
-    this.securityModel = securityModel;
-    this.jwt = jwt;
-    this.bcrypt = bcrypt;
+    this._securityModel = securityModel;
+    this._jwt = jwt;
+    this._bcrypt = bcrypt;
   }
 
   public async bcryptVerify(password: string, hash: string): Promise<boolean> {
-    const isPasswordValid = await this.bcrypt.comparePassword(password, hash);
+    const isPasswordValid = await this._bcrypt.comparePassword(password, hash);
 
     if (!isPasswordValid) throw new CustomError('Senha inválida!', 401);
 
@@ -22,12 +22,12 @@ export default class SecurityService implements ISecurityService {
   }
 
   public verifyJwt(token: string): IJwtPayload {
-    return this.jwt.verifyToken(token);
+    return this._jwt.verifyToken(token);
   }
 
   public async login(loginReq: IUserLogin): Promise<string> {
     const { email, password } = loginReq;
-    const user = await this.securityModel.getUserByEmail(email);
+    const user = await this._securityModel.getUserByEmail(email);
 
     if (!user) throw new CustomError('Conta não encontrada!', 404);
 
@@ -36,23 +36,23 @@ export default class SecurityService implements ISecurityService {
 
     if (!isPasswordValid) throw new CustomError('Senha inválida!', 401);
 
-    const token = this.jwt.createToken({ id: user.id, name: user.name, email: user.email });
+    const token = this._jwt.createToken({ id: user.id, name: user.name, email: user.email });
 
     return token;
   }
 
   public async register(registerReq: IUserRegister): Promise<string> {
     const { name, email, password } = registerReq;
-    const user = await this.securityModel.getUserByEmail(email);
+    const user = await this._securityModel.getUserByEmail(email);
 
     if (user) throw new CustomError('usuário já existe!', 400);
 
-    const encryptedPassword = await this.bcrypt.encryptPassword(password);
-    const createdId = await this.securityModel.register({ name, email, password: encryptedPassword });
+    const encryptedPassword = await this._bcrypt.encryptPassword(password);
+    const createdId = await this._securityModel.register({ name, email, password: encryptedPassword });
 
     if (!createdId) throw new CustomError('Erro ao criar usuário!', 500);
 
-    const token = this.jwt.createToken({ id: createdId, name, email });
+    const token = this._jwt.createToken({ id: createdId, name, email });
 
     return token;
   }
