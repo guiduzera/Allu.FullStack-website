2d48f085e2b19a4455fd52b61d8834e82fe4c683
Guilherme Carvalho <guigui.carvalho15@gmail.com>
2023-09-26 20:16:31 -0300

    FIX: Tirando console.log desnecessário

--- App/frontend/allu-website/src/components/Cart/index.tsx
+++ App/frontend/allu-website/src/components/Cart/index.tsx
@@ -18,7 +18,6 @@ const Cart = () => {
 
     if (cart.length > 0) {
       const quantity = cart.map((item: any) => item.quantity)
-      console.log('aquii', cart[0])
       const totalQuantity = quantity.reduce((acc: any, curr: any) => acc + curr)
       setQuantity(totalQuantity)
     }
