4b0ae55240878ace34e2768a0f8551f51a743470
Guilherme Carvalho <guigui.carvalho15@gmail.com>
2023-09-26 23:57:05 -0300

    TEST: Adicionando mais teste nos métodos da classe ProductsController

--- App/backend/api/src/tests/unit/controller/products.test.ts
+++ App/backend/api/src/tests/unit/controller/products.test.ts
@@ -24,6 +24,25 @@ describe("Testando os métodos da classe ProductsController", () => {
       expect(res.status).toHaveBeenCalledWith(200);
       expect(res.json).toHaveBeenCalledWith({ products: allProductsMock});
     });
+
+    test("Caso ocorra algum erro, deve chamar o next passando o erro", async () => {
+      const productsServiceMock = {
+        getProducts: jest.fn().mockRejectedValue(new Error("Erro")),
+      } as unknown as IProductsService;
+
+      const req = {} as unknown as Request;
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn().mockReturnThis(),
+      } as unknown as Response;
+      const next = jest.fn();
+
+      const productsController = new ProductsController(productsServiceMock);
+
+      await productsController.getProducts(req, res, next);
+
+      expect(next).toHaveBeenCalledWith(new Error("Erro"));
+    });
   });
 
   describe("Testando o método getProductById", () => {
@@ -47,6 +66,25 @@ describe("Testando os métodos da classe ProductsController", () => {
 
       expect(res.status).toHaveBeenCalledWith(200);
       expect(res.json).toHaveBeenCalledWith({ product: { product: allProductsMock.products[0] } });
+    });
+
+    test("Caso ocorra algum erro, deve chamar o next passando o erro", async () => {
+      const productsServiceMock = {
+        getProductById: jest.fn().mockRejectedValue(new Error("Erro")),
+      } as unknown as IProductsService;
+
+      const req = { params: { id: 1 } } as unknown as Request;
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn().mockReturnThis(),
+      } as unknown as Response;
+      const next = jest.fn();
+
+      const productsController = new ProductsController(productsServiceMock);
+
+      await productsController.getProductById(req, res, next);
+
+      expect(next).toHaveBeenCalledWith(new Error("Erro"));
     });
   });
 });
